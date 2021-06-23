f15150cde05a904feafe64f61813d7e14c720521
Gowrishankar <gowrishankar.murali@gmail.com>
2021-06-23 23:16:53 +0200

    Hello World Program

---
+++ Project1/Project1/main.cpp
@@ -0,0 +1,12 @@
+/* First Cpp Code*/
+
+//Includes
+#include <iostream>
+
+int main()
+{
+	//Use namespace if needed, though not the best idea
+	using namespace std;
+
+	cout << "Hello World" << "\n New Day.";
+}
