0df7b73dec2c223b9d3fc107a2406648de6188c0
Gowrishankar <gowrishankar.murali@gmail.com>
2021-06-23 23:57:54 +0200

    UNiform Initialization Example

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -9,4 +9,19 @@ int main()
 	using namespace std;
 
 	cout << "Hello World" << "\n New Day.";
+
+	//UNiform initialization
+	int b1{}; // Value init
+	int b2{ 82 }; //Direct Init
+	char e[8]{}; 
+	char e1[8]{ "Hello" };
+
+	char* p1 = new char[8]{};
+	char* p2 = new char[9]{"Vanakkam"};
+
+	int* p3 = new int{ 21 };
+
+	//Printing all the values
+	cout << "Integersa b1, b2: " << b1 << "\t" << b2 << "\n Char Arrays" << e << "\t" << e1 << "\n" << "Pointers are " << *p1 << "\t" << *p2 << *(p2+1) << "\t" << *p3;
+
 }
