597a2c36e750c11e154807613a82511c684d1e58
Gowrishankar <gowrishankar.murali@gmail.com>
2021-06-24 23:41:19 +0200

    Reference variable Example Assignment 1 iúsing reference variable

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -2,6 +2,10 @@
 
 //Includes
 #include <iostream>
+#include "init_types.h"
+#include "reference_example.h"
+#include "common_func.h"
+
 
 int main()
 {
@@ -10,18 +14,19 @@ int main()
 
 	cout << "Hello World" << "\n New Day.";
 
-	//UNiform initialization
-	int b1{}; // Value init
-	int b2{ 82 }; //Direct Init
-	char e[8]{}; 
-	char e1[8]{ "Hello" };
+	//init_example();
 
-	char* p1 = new char[8]{};
-	char* p2 = new char[9]{"Vanakkam"};
+	//reference_egs();
 
-	int* p3 = new int{ 21 };
+	//Assignment 1
+	int sum, fact, num1{ 21 }, num2{ 32 };
+	Add(21, 32, sum);
+	Factorial(3, fact);
+	Swap(num1, num2);
 
-	//Printing all the values
-	cout << "Integersa b1, b2: " << b1 << "\t" << b2 << "\n Char Arrays" << e << "\t" << e1 << "\n" << "Pointers are " << *p1 << "\t" << *p2 << *(p2+1) << "\t" << *p3;
+	std::cout << " Sum is " << sum << "\nFactorial is " << fact << "\nSwapped values of " << num2 << " and " << num1 << " is " << num1 << " and " << num2;
+
+	return 0;
+
 
 }
